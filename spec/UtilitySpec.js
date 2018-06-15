88564d4ee758643334a04a47ccb6e6c3249ff87e
Tyler <tstanish23@icloud.com>
2018-06-14 20:55:11 -0400

    Added isOver21() utility function

--- spec/UtilitySpec.js
+++ spec/UtilitySpec.js
@@ -1,12 +1,23 @@
-describe("general purpose utility functions", function(){
-	describe("fullName()", function() {
-		it("concatenates two name parts, separated by a space", function(){
+describe("general purpose utility functions", () => {
+	describe("fullName()", () => {
+		it("concatenates two name parts, separated by a space", () => {
 			expect(fullName("Tyler", "Stanish")).toEqual("Tyler Stanish");
 		});
 	});
-	describe("ageInYears()", function() {
-		it("returns age for a birthday string", function(){
+	describe("ageInYears()", () => {
+		it("returns age for a birthday string", () => {
 			expect(ageInYears("1980-06-01")).toEqual(38);
+		});
+	});
+	describe("isOver21()", () => {
+		it("returns true when age is 21", () => {
+			expect(isOver21("1997-06-14")).toBe(true);
+		});
+		it("returns true when age is 21", () => {
+			expect(isOver21("1991-10-23")).toBe(true);
+		});
+		it("returns true when age is 21", () => {
+			expect(isOver21("2000-01-31")).toBe(false);
 		});
 	});
 });
--- src/Utility.js
+++ src/Utility.js
@@ -1,12 +1,17 @@
-function fullName(name1, name2){
+function fullName(name1, name2) {
 	return `${name1} ${name2}`;
 }
 
-function ageInYears(birthdayString){
+function ageInYears(birthdayString) {
 	const birthday = new Date(birthdayString);
 	const ageInMilliseconds = Date.now() - birthday;
 	const millisecondsPerYear = 1000 * 60 * 60 * 24 * 365.25;
 	return Math.floor(ageInMilliseconds / millisecondsPerYear);
-	
 
+
+}
+
+function isOver21(birthdayString) {
+	return ageInYears(birthdayString) >= 21;
+	
 }
